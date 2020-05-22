9034e07d341940368f687e4b5da3e8c95cd7ae83
Alexander Crompton <cromptonwork@alexanders-air.localdomain>
2020-05-22 13:02:35 +0200

    Create fitness timer

---
+++ fitness_timer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+EXERCISES=("Hand walk"
+           "Hip crossover" \
+           "Scorpion" \
+           "Calf Stretch" \
+           "Posture elbow raise" \
+           "Inverted hamstring" \
+           "Lateral lunges" \
+           "Forward lunge with forearm to instep" \
+           "Backward lunge with a twist" \
+           "Drop-lunge" \
+           "Lower back squat-to-stand")
+
+
+DURATION=120
+NEAR_DONE=20
+REST=20
+
+single_exercise () {
+  echo "$1..."
+  say "$1"
+  sleep $((DURATION-NEAR_DONE))
+  say "$NEAR_DONE more seconds"
+  sleep $NEAR_DONE
+  say "Done with that"
+  sleep $REST
+
+}
+
+# Indices used so strings don't get c
+for ((i = 0; i < ${#EXERCISES[@]}; i++)); do
+  single_exercise "${EXERCISES[$i]}" $DURATION
+done
